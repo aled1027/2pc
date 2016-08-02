12e90339c28a15bdd3a44ddca925ca534b13760a
Alex Ledger <a.led1027@gmail.com>
2016-08-01 18:03:25 -0700

    updated run.sh

--- run.sh
+++ run.sh
@@ -2,19 +2,22 @@ NTIMES=100
 CTYPE=STANDARD #CTYPE=SCMC
 mkdir -p logs
 
-for TYPE in LINEAR
+for TYPE in WDBC CREDIT NURSERY_DT ECG_DT WDBC_NB NURSERY_NB AUD_NB
 do
-    #sleep 0.5
-    #./bin/test --chaining $CTYPE --type $TYPE --times $NTIMES --garb-full  2> logs/$TYPE-garb-full.txt &
-    #sleep 0.5
-    #./bin/test --chaining $CTYPE --type $TYPE --times $NTIMES --eval-full 2> logs/$TYPE-eval-full.txt
-    #sleep 1.0
+    sleep 0.5
+    ./bin/test --chaining $CTYPE --type $TYPE --times $NTIMES --garb-full  2> logs/$TYPE-garb-full.txt &
+    sleep 0.5
+    ./bin/test --chaining $CTYPE --type $TYPE --times $NTIMES --eval-full 2> logs/$TYPE-eval-full.txt
+    sleep 1.0
 
     echo -e "\n$TYPE Offline/Online\n"
 
-    #./bin/test --chaining $CTYPE --type $TYPE --garb-off 2> logs/$TYPE-garb-off.txt 1>/dev/null &
-    #sleep 0.5
-    #./bin/test --chaining $CTYPE --type $TYPE --eval-off 2> logs/$TYPE-eval-off.txt 1>/dev/null
+    rm files/garbler_gc/*;
+    rm files/evaluator_gcs/*; 
+
+    ./bin/test --chaining $CTYPE --type $TYPE --garb-off 2> logs/$TYPE-garb-off.txt 1>/dev/null &
+    sleep 0.5
+    ./bin/test --chaining $CTYPE --type $TYPE --eval-off 2> logs/$TYPE-eval-off.txt 1>/dev/null
     echo -e "\n$TYPE Online\n"
     sleep 0.5
     ./bin/test --chaining $CTYPE --type $TYPE --times $NTIMES --garb-on  2> logs/$TYPE-garb-on.txt &
@@ -22,49 +25,3 @@ do
     ./bin/test --chaining $CTYPE --type $TYPE --times $NTIMES --eval-on 2> logs/$TYPE-eval-on.txt
 done
 
-
-
-#for TYPE in AES CBC
-#do
-#    echo -e "\n$TYPE Full\n"
-#
-#    sleep 0.5
-#    ./bin/test --chaining $CTYPE --type $TYPE --times $NTIMES --garb-full  2> logs/$TYPE-garb-full.txt &
-#    sleep 0.5
-#    ./bin/test --chaining $CTYPE --type $TYPE --times $NTIMES --eval-full 2> logs/$TYPE-eval-full.txt
-#    sleep 1.0
-#
-#    echo -e "\n$TYPE Offline/Online\n"
-#
-#    ./bin/test --chaining $CTYPE --type $TYPE --garb-off 2> logs/$TYPE-garb-off.txt 1>/dev/null &
-#    sleep 0.5
-#    ./bin/test --chaining $CTYPE --type $TYPE --eval-off 2> logs/$TYPE-eval-off.txt 1>/dev/null
-#    sleep 0.5
-#    ./bin/test --chaining $CTYPE --type $TYPE --times $NTIMES --garb-on  2> logs/$TYPE-garb-on.txt &
-#    sleep 0.5
-#    ./bin/test --chaining $CTYPE --type $TYPE --times $NTIMES --eval-on 2> logs/$TYPE-eval-on.txt
-#
-#done
-
-#TYPE="LEVEN"
-#for NSYMBOLS in 30
-#do
-#    echo -e "\n$TYPE-$NSYMBOLS Full\n"
-#
-#    ./bin/test --chaining $CTYPE --type $TYPE --nsymbols $NSYMBOLS --times $NTIMES --garb-full  2> logs/$TYPE-garb-full.txt &
-#    sleep 1.0
-#    ./bin/test --chaining $CTYPE --type $TYPE --nsymbols $NSYMBOLS --times $NTIMES --eval-full 2> logs/$TYPE-eval-full.txt
-#    sleep 1.0
-#
-#    echo -e "\n$TYPE-$NSYMBOLS Offline/Online\n"
-#
-#    ./bin/test --chaining $CTYPE --type $TYPE --nsymbols $NSYMBOLS --garb-off 2> logs/$TYPE-garb-off.txt 1>/dev/null &
-#    sleep 1.0
-#    ./bin/test --chaining $CTYPE --type $TYPE --nsymbols $NSYMBOLS --eval-off 2> logs/$TYPE-eval-off.txt 1>/dev/null
-#    sleep 1.0
-#    ./bin/test --chaining $CTYPE --type $TYPE --nsymbols $NSYMBOLS --times $NTIMES --garb-on  2> logs/$TYPE-garb-on.txt &
-#    sleep 1.0
-#    ./bin/test --chaining $CTYPE --type $TYPE --nsymbols $NSYMBOLS --times $NTIMES --eval-on 2> logs/$TYPE-eval-on.txt
-#    sleep 1.0
-#
-#done
