089228e1776ab07616097da52bf29662ed6ceac2
Alex J. Malozemoff <amaloz@cs.umd.edu>
2017-01-16 18:59:21 -0800

    use compgc exe

--- run.sh
+++ run.sh
@@ -2,14 +2,16 @@ NTIMES=10
 CTYPE=STANDARD #CTYPE=SCMC
 mkdir -p logs
 
+prog=./src/compgc
+
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(readlink -f build)/lib
 
 for TYPE in AUD_NB # WDBC CREDIT NURSERY_DT ECG_DT WDBC_NB NURSERY_NB AUD_NB
 do
     sleep 0.5
-    ./bin/test --chaining $CTYPE --type $TYPE --times $NTIMES --garb-full & # 2> logs/$TYPE-garb-full.txt &
+    $prog --chaining $CTYPE --type $TYPE --times $NTIMES --garb-full & # 2> logs/$TYPE-garb-full.txt &
     sleep 0.5
-    ./bin/test --chaining $CTYPE --type $TYPE --times $NTIMES --eval-full # 2> logs/$TYPE-eval-full.txt
+    $prog --chaining $CTYPE --type $TYPE --times $NTIMES --eval-full # 2> logs/$TYPE-eval-full.txt
     sleep 1.0
 
     echo -e "\n$TYPE Offline/Online\n"
@@ -17,13 +19,13 @@ do
     rm files/garbler_gc/*;
     rm files/evaluator_gcs/*; 
 
-    ./bin/test --chaining $CTYPE --type $TYPE --garb-off & # 2> logs/$TYPE-garb-off.txt 1>/dev/null &
+    ./src/compgc --chaining $CTYPE --type $TYPE --garb-off & # 2> logs/$TYPE-garb-off.txt 1>/dev/null &
     sleep 0.5
-    ./bin/test --chaining $CTYPE --type $TYPE --eval-off # 2> logs/$TYPE-eval-off.txt 1>/dev/null
+    ./src/compgc --chaining $CTYPE --type $TYPE --eval-off # 2> logs/$TYPE-eval-off.txt 1>/dev/null
     echo -e "\n$TYPE Online\n"
     sleep 0.5
-    ./bin/test --chaining $CTYPE --type $TYPE --times $NTIMES --garb-on & # 2> logs/$TYPE-garb-on.txt &
+    ./src/compgc --chaining $CTYPE --type $TYPE --times $NTIMES --garb-on & # 2> logs/$TYPE-garb-on.txt &
     sleep 1.5
-    ./bin/test --chaining $CTYPE --type $TYPE --times $NTIMES --eval-on # 2> logs/$TYPE-eval-on.txt
+    ./src/compgc --chaining $CTYPE --type $TYPE --times $NTIMES --eval-on # 2> logs/$TYPE-eval-on.txt
 done
 
